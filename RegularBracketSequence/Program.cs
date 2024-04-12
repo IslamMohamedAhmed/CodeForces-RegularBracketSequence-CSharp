7bb473313559fcabb9c2cb762008d530f5e5e4d4
unknown <islammohamedahmed49@gmail.com>
2024-04-12 22:00:09 +0200

    regular bracket sequence

---
+++ RegularBracketSequence/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+using static System.Convert;
+using static System.Math;
+
+
+string str;
+int counter = 0;
+
+str = ReadLine();
+int size  = str.Length;
+for(int i = 0; i < str.Length; i++)
+{
+    char ch  = str[i];
+    if (ch == '(')
+    {
+        counter++;
+    }
+    else
+    {
+        if(counter == 0) {
+            size--;
+        }
+        else
+        {
+            counter--;
+        }
+    }
+}
+
+WriteLine(size-counter);
